21b456c9baabe34486e147e58695752bb82e3876
sophie <52949826+sophunky@users.noreply.github.com>
2023-06-26 14:30:50 -0400

    craft page updates (new pages instead of pop-up

--- script.js
+++ script.js
@@ -27,59 +27,69 @@ function hover(img)
 // plastic basket popup
 var plasticPopup = document.getElementById("plasticPopup");
 function plasticClicky(div) {
-   plasticPopup.style.display = "block";
+   //plasticPopup.style.display = "block";
+   window.location.href = "plasticbasket.html";
 }
 
 // crochet hanging popup
 var crochetHangingPopup = document.getElementById("crochetHangingPopup");
 function crochetHangingClicky(div) {
-   crochetHangingPopup.style.display = "block";
+   //crochetHangingPopup.style.display = "block";
+   window.location.href = "crochethanging.html";
 }
 
 // pillow popup
 var pillowPopup = document.getElementById("pillowPopup");
 function pillowClicky(div) {
-   pillowPopup.style.display = "block";
+   //pillowPopup.style.display = "block";
+   window.location.href = "pillow.html";
 }
 
 // rug popup
 var rugPopup = document.getElementById("rugPopup");
 function rugClicky(div) {
-   rugPopup.style.display = "block";
+   //rugPopup.style.display = "block";
+   window.location.href = "rugs.html";
 }
 
 // bag popup
 var bagPopup = document.getElementById("bagPopup");
 function bagClicky(div) {
-   bagPopup.style.display = "block";
+   //bagPopup.style.display = "block";
+   window.location.href = "wovenbag.html";
 }
 
 // tetris popup
 var tetrisPopup = document.getElementById("tetrisPopup");
 function tetrisClicky(div) {
-   tetrisPopup.style.display = "block";
+   //tetrisPopup.style.display = "block";
+   window.location.href = "tetris.html";
 }
 
 // close button behavior
 var closeButton = document.getgetElementById("closeButton");
 function escapeClicky(div){
-   plasticPopup.style.display = "none";
+   window.location.href = "index.html";
+   /*plasticPopup.style.display = "none";
    crochetHangingPopup.style.display = "none";
    pillowPopup.style.display = "none";
    rugPopup.style.display = "none";
    bagPopup.style.display = "none";
-   tetrisPopup.style.display = "none";
+   tetrisPopup.style.display = "none";*/
 }
 
 // close pop-up when esc. is pressed (reference: https://jsfiddle.net/m9w8m/)
-document.onkeydown = function(evt) {
+/*document.onkeydown = function(evt) {
    evt = evt || window.event;
-   if (evt.keyCode == 27) {
+
+   
+   if (evt.key == "Escape") {
       plasticPopup.style.display = "none";
       crochetHangingPopup.style.display = "none";
       pillowPopup.style.display = "none";
       rugPopup.style.display = "none";
       bagPopup.style.display = "none";
-      tetrisPopup.style.display = "none";
+   tetrisPopup.style.display = "none";*
+      window.location.href = "index.html";
    }
-};
+};*/
